f0de922ec16e9a4f3a8f640253ae614733d31652
RP5628 <87276017+RP5628@users.noreply.github.com>
2024-02-23 10:41:21 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Sidebar } from "./component/Sidebar";
+import { Navbar } from "./component/Navbar";
+import { Route, Routes } from "react-router-dom";
+import { Login } from "./component/Login";
+import { UserDashBoard } from "./component/UserDashBoard";
+import { Expenses } from "./component/Expenses";
+import { AddExpense } from "./component/AddExpense";
 
 function App() {
+  const path = window.location.pathname;
+  console.log(path);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+      </Routes>
+      {path === "/" || path === "/login" || path === "" ? null : (
+        <div className="wrapper">
+          {path === "/" || path === "/login" || path === "" ? null : (
+            <Sidebar />
+          )}
+          <div className="main-panel">
+            {path === "/" || path === "/login" || path === "" ? null : (
+              <Navbar />
+            )}
+            <div className="content">
+              <Routes>
+                <Route
+                  path="/user/dashboard"
+                  element={<UserDashBoard />}
+                ></Route>
+                <Route path="/user/expenses" element={<Expenses />}></Route>
+                <Route path="/expense/form" element={<AddExpense />}></Route>
+
+              </Routes>
+            </div>
+          </div>
+        </div>
+      )}
+    </body>
   );
 }
 
